565bec44d1406190e77832348546741925e85c48
m-sarabi <m.sarabi.jkd@gmail.com>
2024-07-19 05:23:25 +0330

    finally got a working image palette

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let activeTool;
+let paletteMaxSize = 10;
+
 class Gradient {
     constructor(color1, color2, number) {
         this.color1 = color1;
@@ -22,36 +25,130 @@ class Gradient {
         return gradientColors;
     }
 
-    createColorBox(color) {
-        const colorBox = document.createElement('div');
-        colorBox.classList.add('color-box');
-        colorBox.style.backgroundColor = color.rgb();
-        colorBox.addEventListener('click', () => {
-            navigator.clipboard.writeText(color.hex()).then();
-        });
-        return colorBox;
-    }
-
-    createCopyButton() {
-        const button = document.createElement('div');
-        button.classList.add('copy-btn');
-        const text = document.createElement('span');
-        text.innerHTML = 'copy';
-        button.appendChild(text);
-        return button;
-    }
-
     get gradientElements() {
         const gradientColors = this.calculateGradient();
         const gradientDivs = [];
         gradientColors.forEach((color) => {
-            gradientDivs.push(this.createColorBox(color));
+            gradientDivs.push(createColorBox(color));
         });
         gradientDivs.forEach((gradientDiv) => {
-            gradientDiv.append(this.createCopyButton());
+            gradientDiv.append(createCopyButton());
         });
         return gradientDivs;
     }
+}
+
+class ImagePalette {
+    constructor(file) {
+        this.file = file;
+    }
+
+    async getPixelsArray(file) {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = function (event) {
+                const img = new Image();
+                img.src = event.target.result;
+                img.onload = function () {
+                    const canvas = document.createElement('canvas');
+                    let d = 1;
+                    if (img.width * img.height > 600 * 600) {
+                        d = Math.sqrt(img.width * img.height / (600 * 600));
+                    }
+                    canvas.width = img.width / d;
+                    canvas.height = img.height / d;
+                    const ctx = canvas.getContext('2d');
+                    ctx.drawImage(img, 0, 0);
+                    const data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+                    const pixelArray = [];
+                    for (let i = 0; i < data.length; i += 4) {
+                        pixelArray.push([data[i], data[i + 1], data[i + 2]]);
+                    }
+                    resolve(pixelArray);
+                };
+            };
+            reader.onerror = reject;
+            reader.readAsDataURL(file);
+        });
+    }
+
+    async updatePreview() {
+        const canvas = document.getElementById('image-preview');
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        const img = new Image();
+        img.src = URL.createObjectURL(this.file);
+        img.onload = () => {
+            canvas.width = 300;
+            canvas.height = img.height * 300 / img.width;
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        };
+    }
+
+    async getPalette() {
+        const pixelArray = await this.getPixelsArray(this.file);
+        let kMeans = new KMeans(paletteMaxSize, pixelArray);
+        let palette = kMeans.getClusters();
+        let palettePercentages = [];
+        for (let i in palette) {
+            palettePercentages.push([new Color(...palette[i].color), Math.round((palette[i].count / pixelArray.length) * 10000) / 100]);
+        }
+        palettePercentages.sort((a, b) => b[1] - a[1]);
+        return palettePercentages;
+    }
+
+    get gradientElements() {
+        return new Promise(
+            (resolve, reject) => {
+                try {
+                    this.getPalette().then(
+                        (palette) => {
+                            palette = palette.filter((color) => color[1] > 1);
+                            const gradientDivs = [];
+                            palette.forEach((color) => {
+                                gradientDivs.push(createColorBox(color[0]));
+                            });
+                            gradientDivs.forEach((gradientDiv, index) => {
+                                gradientDiv.style.flex = `${palette[index][1]} 1`;
+                                gradientDiv.append(createCopyButton());
+                            });
+                            gradientDivs.forEach((gradientDiv, index) => {
+                                gradientDiv.addEventListener('mouseover', () => {
+                                    gradientDiv.style.flex = `${palette[index][1] * 2} 1`;
+                                });
+                                gradientDiv.addEventListener('mouseout', () => {
+                                    gradientDiv.style.flex = `${palette[index][1]} 1`;
+                                });
+                            });
+                            resolve(gradientDivs);
+                        }
+                    );
+                } catch (e) {
+                    reject(e);
+                }
+            }
+        );
+
+    }
+}
+
+function createColorBox(color) {
+    const colorBox = document.createElement('div');
+    colorBox.classList.add('color-box');
+    colorBox.style.backgroundColor = color.rgb();
+    colorBox.addEventListener('click', () => {
+        navigator.clipboard.writeText(color.hex()).then();
+    });
+    return colorBox;
+}
+
+function createCopyButton() {
+    const button = document.createElement('div');
+    button.classList.add('copy-btn');
+    const text = document.createElement('span');
+    text.innerHTML = 'copy';
+    button.appendChild(text);
+    return button;
 }
 
 function updateGradient() {
@@ -63,54 +160,98 @@ function updateGradient() {
     document.querySelector('#gradient-colors').append(...gradient.gradientElements);
 }
 
+async function updateImagePalette(file) {
+    const imagePalette = new ImagePalette(file);
+    document.querySelector('#image-palette-colors').innerHTML = "";
+    document.querySelector('#image-palette-colors').append(...await imagePalette.gradientElements);
+    document.getElementById('palette-image-file').value = "";
+}
+
 function initEvents() {
     document.addEventListener('input', (e) => {
-            console.log(e.target.type);
-            if (e.target.nodeName === 'INPUT' && e.target.type === 'color') {
-                switch (e.target.id) {
-                    case 'gradient-color1':
-                    case 'gradient-color2':
-                        e.target.parentNode.style.background = e.target.value;
-                        break;
-                }
-            } else if (e.target.nodeName === 'INPUT' && e.target.type === 'number') {
-                if (e.target.hasAttribute('max')) {
-                    if (parseInt(e.target.value) > parseInt(e.target.getAttribute('max'))) {
-                        e.target.value = e.target.getAttribute('max');
+            if (activeTool === 'gradient-tool') {
+                if (e.target.nodeName === 'INPUT' && e.target.type === 'color') {
+                    switch (e.target.id) {
+                        case 'gradient-color1':
+                        case 'gradient-color2':
+                            e.target.parentNode.style.background = e.target.value;
+                            break;
+                    }
+                } else if (e.target.nodeName === 'INPUT' && e.target.type === 'number') {
+                    if (e.target.hasAttribute('max')) {
+                        if (parseInt(e.target.value) > parseInt(e.target.getAttribute('max'))) {
+                            e.target.value = e.target.getAttribute('max');
+                        }
+                    }
+                    if (e.target.hasAttribute('min')) {
+                        if (parseInt(e.target.value) < parseInt(e.target.getAttribute('min'))) {
+                            e.target.value = e.target.getAttribute('min');
+                        }
+                    }
+                    switch (e.target.id) {
+                        case 'gradient-number':
+                            document.querySelector('#gradient-range').value = e.target.value;
+                            break;
+                    }
+                } else if (e.target.nodeName === 'INPUT' && e.target.type === 'range') {
+                    switch (e.target.id) {
+                        case 'gradient-range':
+                            document.querySelector('#gradient-number').value = e.target.value;
+                            break;
                     }
                 }
-                if (e.target.hasAttribute('min')) {
-                    if (parseInt(e.target.value) < parseInt(e.target.getAttribute('min'))) {
-                        e.target.value = e.target.getAttribute('min');
+                updateGradient();
+            } else if (activeTool === 'image-palette-tool') {
+                if (e.target.nodeName === 'INPUT' && e.target.type === 'number') {
+                    if (e.target.hasAttribute('max')) {
+                        if (parseInt(e.target.value) > parseInt(e.target.getAttribute('max'))) {
+                            e.target.value = e.target.getAttribute('max');
+                        }
                     }
-                }
-                switch (e.target.id) {
-                    case 'gradient-number':
-                        document.querySelector('#gradient-range').value = e.target.value;
-                        break;
-                }
-            } else if (e.target.nodeName === 'INPUT' && e.target.type === 'range') {
-                switch (e.target.id) {
-                    case 'gradient-range':
-                        document.querySelector('#gradient-number').value = e.target.value;
-                        break;
+                    if (e.target.hasAttribute('min')) {
+                        if (parseInt(e.target.value) < parseInt(e.target.getAttribute('min'))) {
+                            e.target.value = e.target.getAttribute('min');
+                        }
+                    }
+                    switch (e.target.id) {
+                        case 'palette-number':
+                            document.querySelector('#palette-range').value = e.target.value;
+                            paletteMaxSize = e.target.value;
+                            break;
+                    }
+                } else if (e.target.nodeName === 'INPUT' && e.target.type === 'range') {
+                    switch (e.target.id) {
+                        case 'palette-range':
+                            document.querySelector('#palette-number').value = e.target.value;
+                            paletteMaxSize = e.target.value;
+                            break;
+                    }
                 }
             }
-            updateGradient();
         }
     );
     document.addEventListener('click', (e) => {
         if (e.target.classList.contains('sidebar-btn')) {
             hideAllTools();
             switch (e.target.id) {
-                case 'gradient-tool':
+                case 'gradient-btn':
                     document.querySelector('#gradient-tools').style.display = 'flex';
+                    activeTool = 'gradient-tool';
                     break;
 
-                case 'color-picker':
-                    document.querySelector('#color-picker-tools').style.display = 'flex';
+                case 'image-palette-btn':
+                    document.querySelector('#image-palette-tools').style.display = 'flex';
+                    activeTool = 'image-palette-tool';
                     break;
             }
+        }
+    });
+    document.addEventListener('change', async function (e) {
+        if (e.target.id === 'palette-image-file') {
+            const file = e.target.files[0];
+            const imagePalette = new ImagePalette(file);
+            await imagePalette.updatePreview();
+            updateImagePalette(file).then();
         }
     });
 }
@@ -124,6 +265,7 @@ function hideAllTools() {
 
 
 function init() {
+    activeTool = 'gradient-tool';
     hideAllTools();
     document.querySelector('#gradient-tools').style.display = 'flex';
     initEvents();
--- js/utils.js
+++ js/utils.js
@@ -31,3 +31,122 @@ class Color {
         return `#${zeroPad(this.r)}${zeroPad(this.g)}${zeroPad(this.b)}`;
     }
 }
+
+class KMeans {
+    constructor(k, colors) {
+        this.k = k;
+        this.colors = colors;
+        this.clusters = [];
+        this.assignments = new Array(colors.length);
+        this.converged = false;
+        this.count = 0;
+    }
+
+    kMeansPlusPlus() {
+        const n = this.colors.length;
+        const distances = new Array(n).fill(Infinity);
+
+        this.clusters.push(this.colors[Math.floor(Math.random() * n)]);
+
+        for (let i = 1; i < this.k; i++) {
+            let sum = 0;
+            for (let j = 0; j < n; j++) {
+                distances[j] = Math.min(distances[j], this.getDistance(this.colors[j], this.clusters[i - 1]));
+                sum += distances[j];
+            }
+
+            let target = Math.random() * sum;
+            for (let j = 0; j < n; j++) {
+                target -= distances[j];
+                if (target <= 0) {
+                    this.clusters.push(this.colors[j]);
+                    break;
+                }
+            }
+        }
+    }
+
+    getDistance(color1, color2) {
+        const r = color1[0] - color2[0];
+        const g = color1[1] - color2[1];
+        const b = color1[2] - color2[2];
+        return Math.sqrt(r * r + g * g + b * b);
+    }
+
+    meanClusterColor(cluster) {
+        const total = cluster.reduce((sum, color) => {
+            return [
+                sum[0] + color[0],
+                sum[1] + color[1],
+                sum[2] + color[2]
+            ];
+        }, [0, 0, 0]);
+        return [
+            Math.round(total[0] / cluster.length),
+            Math.round(total[1] / cluster.length),
+            Math.round(total[2] / cluster.length)
+        ];
+    }
+
+    clusterPixels() {
+        for (let i = 0; i < this.colors.length; i++) {
+            let minDistance = Infinity;
+            let closestCluster = -1;
+            for (let j = 0; j < this.clusters.length; j++) {
+                const distance = this.getDistance(this.colors[i], this.clusters[j]);
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    closestCluster = j;
+                }
+            }
+            this.assignments[i] = closestCluster;
+        }
+    }
+
+    updateClusters() {
+        const newClusters = Array.from({length: this.k}, () => []);
+        for (let i = 0; i < this.colors.length; i++) {
+            newClusters[this.assignments[i]].push(this.colors[i]);
+        }
+
+        this.converged = true;
+        for (let i = 0; i < this.clusters.length; i++) {
+            const meanColor = this.meanClusterColor(newClusters[i]);
+            if (this.getDistance(this.clusters[i], meanColor) > 0) {
+                this.converged = false;
+                this.clusters[i] = meanColor;
+            }
+        }
+    }
+
+    run() {
+        this.kMeansPlusPlus();
+        while (!this.converged) {
+            this.clusterPixels();
+            this.updateClusters();
+        }
+    }
+
+    getClusters() {
+        this.run();
+        const colorCounts = this.clusters.map(() => 0);
+        for (let i = 0; i < this.colors.length; i++) {
+            colorCounts[this.assignments[i]]++;
+        }
+        return this.clusters.map((color, index) => {
+            return {
+                'color': color,
+                'count': colorCounts[index]
+            };
+        });
+    }
+}
+
+const random = {
+    randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    choice(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
+    },
+};
